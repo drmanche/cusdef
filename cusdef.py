e0af9c67342c16fb36ee60aeaa38c43aa03f0ea1
drmanche <82394874+drmanche@users.noreply.github.com>
2023-10-04 16:01:13 -0500

    Update cusdef.py

--- cusdef.py
+++ cusdef.py
@@ -12,3 +12,11 @@ def dideco(text:str) -> str:
 def intext(text:str) -> str:
     '''Invierte una oracion'''
     return text[::-1]
+
+def palincheck(input_string:str) -> bool:
+    return input_string.lower()[::-1] == input_string.lower()
+
+def ordanilist(my_list:list) -> list:
+    new_list = [i for element in my_list for i in element]
+    new_list.sort(reverse=True)
+    return new_list
